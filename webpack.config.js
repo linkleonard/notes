7fcd4a0aca1616891b654a29d1141ef2d5d3c072
linkleonard <leon.wlaw@gmail.com>
2016-09-29 19:01:49 -0400

    Add eslint to project

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,21 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  entry: './src/index.jsx',
-  output: {
-    path: './bin',
-    filename: 'app.bundle.js'
-  },
-  devServer: {
-    contentBase: './bin',
-  },
-  module: {
-    loaders: [
-      {
-        test: /.jsx?$/,
-        loader: 'babel-loader',
-        exclude: path.join(__dirname, 'node_modules'),
-      }
-    ]
-  }
+    entry: './src/index.jsx',
+    output: {
+        path: './bin',
+        filename: 'app.bundle.js'
+    },
+    devServer: {
+        contentBase: './bin',
+    },
+    module: {
+        loaders: [
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: path.join(__dirname, 'node_modules'),
+            }
+        ]
+    }
 }
