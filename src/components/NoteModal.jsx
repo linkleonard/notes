30b0739499dc1e5ef538fc54d213cb55f55a7e16
linkleonard <leon.wlaw@gmail.com>
2016-10-04 01:20:53 -0400

    Add note editor modal header

--- src/components/NoteModal.jsx
+++ src/components/NoteModal.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { Modal } from 'react-bootstrap';
 import NoteForm from '../containers/NoteForm.jsx';
 
-const NoteModal = ({ show, handleSubmit }) => (
+const NoteModal = ({ show, index, handleSubmit }) => (
     <Modal show={ show } >
+        <Modal.Header>
+            <h4>{ index === null ? 'Create new' : 'Update' } Note</h4>
+        </Modal.Header>
         <Modal.Body>
             <NoteForm onSubmit={ handleSubmit } />
         </Modal.Body>
